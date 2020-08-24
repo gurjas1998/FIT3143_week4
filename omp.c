b9e983c1c9d1b4317a6a1f60ad851bd3cc565f80
tnngu64 <69128698+tnngu64@users.noreply.github.com>
2020-08-24 11:19:42 +1000

    Update omp.c

--- omp.c
+++ omp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <omp.h>
+#include <time.h>
 
 
 int main() {
@@ -10,41 +10,41 @@ int main() {
     int sum_wins = 0;
     int display_size = 10;
     int number_size = 26;
-    int* pDisplay = (int*)malloc(display_size * sizeof(int)); // for some reason, initialising a stack array generated random number elements??
-    int* pNumber = (int*)malloc(number_size * sizeof(int)); // whereas going heap method via malloc generated with 0s...
+    int* pDisplay = (int*)malloc(display_size * sizeof(int));
+    int* pNumber =  (int*)malloc(number_size * sizeof(int));
     
     srand(time(NULL));
     
+    //can run a pragma here as well where we initialise all array elements to 0 
     for(i = 0; i < number_size; i++) {
-        printf("%d\n", pNumber[i]);
+        pNumber[i] = 0;
     }
-    #pragma omp parallel for num_threads(4) schedule(static, 2) // if i get rid of this pragma then the code works fine, but when it's there, pNumber has random numbers????
-    // where to insert the pragma stuff?
     
+    #pragma omp parallel for num_threads(4) schedule(static, 2) 
+    //writing pragma omp parallel for will work directly on the first for loop it sees only.
+    //if we want the following for loops to run in parallel, have to specify #pragma again   
+    for(i = 0; i < display_size; i++) {
+        val = rand() % 25 + 1; // this will generate a random number from 0 to 24 and then add 1 to get (1, 25)
+        pDisplay[i] = val;
+        pNumber[val] += 1;
+        //printf("display %d: %d\n", i+1, pDisplay[i]); //this will print these statements out of order because it's being run in the parallel loop
+        
+    }
     
-        for(i = 0; i < display_size; i++) {
-            val = rand() % 25 + 1; // this will generate a random number from 0 to 24 and then add 1 to get (1, 25)
-            pDisplay[i] = val;
-            pNumber[val] += 1;
-            //printf("pNumber[%d] is %d\n", i, pNumber[i]);
-            printf("display %d: %d\n", i+1, pDisplay[i]);
-        }
-    
-    
+    for(i = 0; i < display_size; i++) {
+        printf("display %d: %d\n", i+1, pDisplay[i]);    
+    }
     
     for(i = 0; i < number_size; i++) {
-        printf("number %d: %d times\n", i, pNumber[i]);
+        //printf("number %d: %d times\n", i, pNumber[i]);
         if(pNumber[i] > 1) {
             //printf("%d appeared %d times\n", i, pNumber[i]);
             sum_wins += 1;
         }
     }
-    for(i = 0; i < number_size; i++) {
-        /**if(pNumber[i] > 1) {
-            printf("%d appeared %d times\n", i, pNumber[i]);
-        }*/
-    }
+    
     printf("number of wins: %d\n", sum_wins);
+    
     free(pDisplay);
     free(pNumber);
     
